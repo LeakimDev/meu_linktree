89e0b5786f21933f09ce378d5e348228e887378e
LeakimDev <mikaeleakim2001@gmail.com>
2026-02-14 23:11:42 -0300

    Refactor script.js for improved readability and functionality

--- script.js
+++ script.js
@@ -25,3 +25,46 @@ setTimeout(escrever,80);
 escrever();
 
 // saBOUR java script
+
+// ===== FUNDO BINARIO CAINDO =====
+const canvas = document.getElementById("matrix");
+const ctx = canvas.getContext("2d");
+
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+
+const letras = "01";
+const letrasArray = letras.split("");
+
+const fontSize = 14;
+const columns = canvas.width / fontSize;
+
+const drops = [];
+for(let x = 0; x < columns; x++)
+drops[x] = 1;
+
+function draw(){
+ctx.fillStyle = "rgba(0,0,0,0.05)";
+ctx.fillRect(0,0,canvas.width,canvas.height);
+
+ctx.fillStyle = "#22c55e"; // cor verde
+ctx.font = fontSize + "px monospace";
+
+for(let i = 0; i < drops.length; i++){
+const text = letrasArray[Math.floor(Math.random()*letrasArray.length)];
+ctx.fillText(text, i*fontSize, drops[i]*fontSize);
+
+if(drops[i]*fontSize > canvas.height && Math.random() > 0.975)
+drops[i] = 0;
+
+drops[i]++;
+}
+}
+
+setInterval(draw, 33);
+
+// responsivo
+window.addEventListener("resize", ()=>{
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+});
